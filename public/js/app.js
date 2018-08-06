b504aa6221cf807bebc2732311d79ca2baa0e0b1
Tawane Paula <tawanepaula8@gmail.com>
2018-08-06 17:21:27 -0300

    Rotas

--- public/js/app.js
+++ public/js/app.js
@@ -10,30 +10,42 @@ app.config(function($routeProvider, $locationProvider) {
             controller: 'mainController'
         })
 
-        .when('/pacientesSuspenso', {
-            templateUrl: './views/pacientesSuspenso.html',
-            controller: 'mainController'
-        })
-
-        .when('/contatos', {
-            templateUrl: './views/contatos.html',
-            controller: 'mainController'
-        })
-
-        .when('/faleconosco', {
-            templateUrl: './views/faleconosco.html',
-            controller: 'mainController'
-        })
-
         .when('/cadastropaciente', {
             templateUrl: './views/cadastropaciente.html',
             controller: 'mainController'
         })
 
-        .when('/portal', {
-            templateUrl: './views/portal.html',
+        .when('/cadastropacientesarquivados', {
+            templateUrl: './views/cadastropacientesarquivados.html',
+            controller: 'mainController'
+        })
+
+        .when('/pacientessuspenso', {
+            templateUrl: './views/pacientessuspenso.html',
+            controller: 'mainController'
+        })
+
+        .when('/pesquisarpacientes', {
+            templateUrl: './views/pesquisarpacientes.html',
+            controller: 'mainController'
+        })
+
+        .when('/procedimentosinvasivos', {
+            templateUrl: './views/vigilancia.html',
+            controller: 'mainController'
+        })
+
+        .when('/antimicrobianos', {
+            templateUrl: './views/antimicrobianos.html',
+            controller: 'mainController'
+        })
+
+        .when('/culturas', {
+            templateUrl: './views/culturas.html',
             controller: 'mainController'
         });
+
+
 
     app.controller('appController', function ($scope, $http) {
     });
